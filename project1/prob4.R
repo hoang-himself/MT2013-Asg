0ae3fbc9af47f2bedb1508b05f0fc4c277b29259
Nguyễn Chính Khôi <57392384+simplynormal@users.noreply.github.com>
2021-05-13 16:17:10 +0700

    Add files via upload

---
+++ project1/prob4.R
@@ -0,0 +1,32 @@
+# Import the data
+file_path <- 'resources/ex4.csv'
+type <- read.csv(file = file_path, sep = ",", row.names = 1, stringsAsFactors = FALSE, check.names = FALSE)
+data <- as.matrix(type)
+
+# Computing Chi-square
+chisq <- chisq.test(data) 
+
+# Print observed counts & expected counts
+print(chisq$observed)
+print(round(chisq$expected, 2))
+
+# Retrieving value
+alpha = 0.05
+X_squared = chisq$statistic # Statistic
+df = chisq$parameter        # Degree of freedom
+pval = chisq$p.value        # P-value
+c = qchisq(1 - alpha, df)    # Computing critical point
+
+# Check for rejection by comparing with critical point
+ifelse(
+  X_squared > c,
+  "Reject H0 by comparing with critical point",
+  "Accept H0 by comparing with critical point"
+)
+
+#Check for rejection by comparing with significance level
+ifelse(
+  pval < alpha,
+  "Reject H0 by comparing with significance level",
+  "Accept H0 by comparing with significance level"
+)
