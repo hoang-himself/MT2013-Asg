b3cdc3396f570658c3cf6464ad902992a60b9e24
ThanhTDX <67357716+ThanhTDX@users.noreply.github.com>
2021-05-17 07:13:32 +0700

    Add files via upload

---
+++ project1/prob3.R
@@ -0,0 +1,12 @@
+#Import the data
+Ques3_dataset <- read.csv(file = "C:/Users/trant/Desktop/file/ex3.csv")
+print(Ques3_dataset)
+
+#Create a two-way ANOVA
+av <- aov(value ~ day_of_week + highschool,data=Ques3_dataset)
+
+#Summarize two-way ANOVA
+print(summary(av))
+
+#Check critical value
+print(qf(0.99,3,9))
