a0529168ed00b74456a34e8ed553005da0e8a58b
simplynormal <simplynormal@users.noreply.github.com>
2021-05-18 14:33:53 +0700

    Refactor comments

--- project2/Linear_regression.R
+++ project2/Linear_regression.R
@@ -2,20 +2,20 @@
 rm(list = ls()) 
 
 ######### 1. Import data #########
-# set working directory
+# Set working directory
 # setwd("D:/HCMUT/HK202/Statistics and Probabilities/BTL")
 
-# install packages to use
+# Install packages to use
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(pacman, rio)
 
-# import file
+# Import file
 grade_csv <- import("grade.csv")
 
-# choose useful information
+# Choose useful information
 grade_csv <- subset(grade_csv,select =c(sex,age,studytime,failures,higher,absences,G1,G2,G3))
 
-# show the table after choose subset
+# Show the table after choose subset
 head(grade_csv)
 
 ######## 2. Data cleaning ########
@@ -35,7 +35,7 @@ sum(is.na(grade_csv))
 ######### 3. Descriptive Statistics #########
 ### ----- a. Transformation ----- ###
 
-# check correlation between G1~G3 G2~G3 to decide which 
+# Check correlation between G1~G3 G2~G3 to decide which 
 # transformation to use or not use transformation
 pairs(G3~G1, grade_csv)
 pairs(G3~G2, grade_csv)
@@ -43,16 +43,15 @@ pairs(G3~G2, grade_csv)
 ### ----- b. Descriptive statistic ----- ###
 
 # ----> For continuous variable <----
-
-# choose column
+# Choose column
 con_var <- c(6,7,8,9)
-# calculate some statistic value
+# Calculate some statistic value
 mean <- apply(grade_csv[,con_var], 2, mean)
 median <- apply(grade_csv[,con_var], 2, median)
 sd <- apply(grade_csv[,con_var], 2, sd)
 min <- apply(grade_csv[,con_var], 2, min)
 max <- apply(grade_csv[,con_var], 2, max)
-# put it all data frame
+# Put it all in data frame
 con_table <- t(data.frame(mean,median,sd,min,max))
 
 con_table
@@ -75,13 +74,13 @@ cat_higher
 
 ### ----- c. Plotting graph ----- ###
 
-# install some package to plot more beautiful
+# Install some package to plot more beautiful
 if (!require("gridExtra")) install.packages("gridExtra")
 library(gridExtra)
 if (!require("ggplot2")) install.packages("ggplot2")
 library(ggplot2)
 
-# histogram plot
+# Histogram plot
 grid.arrange(
   ggplot(grade_csv, aes(sex)) + geom_histogram(stat = "count"),
   ggplot(grade_csv, aes(age)) + geom_histogram(stat = "count"),
@@ -95,14 +94,14 @@ grid.arrange(
   
   ncol=3)
 
-# box plot of G3 for sex, age, studytime, failures, higher
+# Box plot of G3 for sex, age, studytime, failures, higher
 boxplot(G3~sex, grade_csv, main = "Distribution of G3 for each sex")
 boxplot(G3~age, grade_csv, main = "Distribution of G3 for each age")
 boxplot(G3~studytime, grade_csv, main = "Distribution of G3 for each studytime")
 boxplot(G3~failures, grade_csv, main = "Distribution of G3 for each failures")
 boxplot(G3~higher, grade_csv, main = "Distribution of G3 for each higher")
 
-# pair graph
+# Pair graph
 pairs(G3~G2,grade_csv)
 pairs(G3~G1,grade_csv)
 pairs(G3~age,grade_csv)
@@ -113,18 +112,18 @@ pairs(G3~absences,grade_csv)
 
 ############ 4. Fitting linear regression model ###############
 
-# create model
+# Create model
 linearModel <- lm(G3~sex + age + studytime + failures + higher + absences + G1 + G2, data=grade_csv)
 summary(linearModel)
 
 
-######## selecting model properly #########
+######## Selecting model properly #########
 plot(linearModel)
 plot(linear1)
 
 ####### Predict ##########
 
-## function to check fail or pass
+## Function to check fail or pass
 failpass <- function(x){
   if(x >= 10) return("Pass")
   else        return("Fail")
@@ -140,17 +139,17 @@ View(grade_csv)
 if (!require("tidyverse")) install.packages("tidyverse")
 library(tidyverse)
 
-## create a new table and add predict column to a new table
+## Create a new table and add predict column to a new table
 new_grade <- grade_csv %>% select(sex, age, studytime, failures, higher, absences, G1, G2, G3)
 predict_grade <- predict(linear1)
 new_grade <- cbind(new_grade, predict_grade)
 
-## check fail or pass of prediction in new table
+## Check fail or pass of prediction in new table
 evaluate <- c(apply(new_grade["predict_grade"], MARGIN = 1, FUN = failpass))
 new_grade <- cbind(new_grade, evaluate)
 
 
-## create a data frame to compare between G3(real data) and predicted value
+## Create a data frame to compare between G3(real data) and predicted value
 evaluate1 = prop.table(table(grade_csv$evaluate == "Pass"))
 evaluate2 = prop.table(table(new_grade$evaluate == "Pass"))
 Output = data.frame(cbind(evaluate1, evaluate2))
