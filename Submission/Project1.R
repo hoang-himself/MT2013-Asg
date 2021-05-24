f5de9ec1aaf9fe07163ad6226c48791393deff0e
Hoang NGUYEN <baauco@gmail.com>
2021-05-24 10:39:20 +0700

    RC2

--- Submission/Project1.R
+++ Submission/Project1.R
@@ -17,54 +17,57 @@ print(summary(av))
 
 ############
 
+rm(list = ls())
+if (!require("readxl"))
+  install.packages("readxl")
+library("readxl")
+
 # Import the data
-data_file <- read.csv(file="ex1.csv")
+data_file <- read_excel("data.xlsx", sheet = "Sheet2")
 
 # Extract group names to data frame
 fr_gr_names <- data.frame(unique(data_file$group))
 
 # Variables that aid calculations
-fr_gr_sums <- aggregate(data_file$value~data_file$group,
+fr_gr_sums <- aggregate(data_file$value ~ data_file$group,
                         fr_gr_names, sum)
-fr_gr_quans <- aggregate(data_file$value~data_file$group,
+fr_gr_quans <- aggregate(data_file$value ~ data_file$group,
                          fr_gr_names, length)
 gr_sums <- fr_gr_sums$`data_file$value`
 gr_quans <- fr_gr_quans$`data_file$value`
 
 # Degree of freedom
 N <- length(data_file$value)
-F <- length(unique(data_file$group))
-df_f <- F - 1
-df_e <- N - F
+i <- length(unique(data_file$group))
+df_tr <- i - 1
+df_e <- N - i
 df_t <- N - 1
 
 # Sums of squares
 SST <- sum(data_file$value^2) - sum(data_file$value)^2 / N
-SSF <- sum(gr_sums^2 / gr_quans) - sum(data_file$value)^2 / N
-SSE <- SST - SSF
+SSTr <- sum(gr_sums^2 / gr_quans) - sum(data_file$value)^2 / N
+SSE <- SST - SSTr
 
-# Means of sums of squares
-MSF <- SSF / df_f
+# Means of squares
+MSTr <- SSTr / df_tr
 MSE <- SSE / df_e
 
-F <- MSF / MSE
+F <- MSTr / MSE
 
 # Console output
 cat("Sums", gr_sums, "\n")
 cat("Averages", gr_sums / gr_quans, "\n")
 cat("Overall sum", sum(gr_sums), "\n")
 cat("Overall mean", mean(gr_sums / gr_quans), "\n")
-cat("df_f", df_f, "\n")
+cat("df_tr", df_tr, "\n")
 cat("df_e", df_e, "\n")
 cat("df_t", df_t, "\n")
-cat("SSF", SSF, "\n")
+cat("SSTr", SSTr, "\n")
 cat("SSE", SSE, "\n")
 cat("SST", SST, "\n")
-cat("MSF", MSF, "\n")
+cat("MSTr", MSTr, "\n")
 cat("MSE", MSE, "\n")
 cat("F", F, "\n")
-
-cat("F Crit", qf(0.97,4,24), "\n")
 
 
 
--- project1/prob1.R
+++ project1/prob1.R
@@ -12,52 +12,52 @@ print(summary(av))
 
 ############
 
-# # Clear environment
-# rm(list = ls())
+# Clear environment
+rm(list = ls())
 
-# # Import the data
-# data_file <- read.csv(file = "resources/ex1.csv")
+# Import the data
+data_file <- read.csv(file = "resources/ex1.csv")
 
-# # Extract group names to data frame
-# fr_gr_names <- data.frame(unique(data_file$group))
+# Extract group names to data frame
+fr_gr_names <- data.frame(unique(data_file$group))
 
-# # Variables that aid calculations
-# fr_gr_sums <- aggregate(data_file$value ~ data_file$group,
-#                         fr_gr_names, sum)
-# fr_gr_quans <- aggregate(data_file$value ~ data_file$group,
-#                          fr_gr_names, length)
-# gr_sums <- fr_gr_sums$`data_file$value`
-# gr_quans <- fr_gr_quans$`data_file$value`
+# Variables that aid calculations
+fr_gr_sums <- aggregate(data_file$value ~ data_file$group,
+                        fr_gr_names, sum)
+fr_gr_quans <- aggregate(data_file$value ~ data_file$group,
+                         fr_gr_names, length)
+gr_sums <- fr_gr_sums$`data_file$value`
+gr_quans <- fr_gr_quans$`data_file$value`
 
-# # Degree of freedom
-# N <- length(data_file$value)
-# i <- length(unique(data_file$group))
-# df_tr <- i - 1
-# df_e <- N - i
-# df_t <- N - 1
+# Degree of freedom
+N <- length(data_file$value)
+i <- length(unique(data_file$group))
+df_tr <- i - 1
+df_e <- N - i
+df_t <- N - 1
 
-# # Sums of squares
-# SST <- sum(data_file$value ^ 2) - sum(data_file$value) ^ 2 / N
-# SSTr <- sum(gr_sums ^ 2 / gr_quans) - sum(data_file$value) ^ 2 / N
-# SSE <- SST - SSTr
+# Sums of squares
+SST <- sum(data_file$value^2) - sum(data_file$value)^2 / N
+SSTr <- sum(gr_sums^2 / gr_quans) - sum(data_file$value)^2 / N
+SSE <- SST - SSTr
 
-# # Means of sums of squares
-# MSTr <- SSTr / df_tr
-# MSE <- SSE / df_e
+# Means of squares
+MSTr <- SSTr / df_tr
+MSE <- SSE / df_e
 
-# F <- MSTr / MSE
+F <- MSTr / MSE
 
-# # Console output
-# cat("Sums", gr_sums, "\n")
-# cat("Averages", gr_sums / gr_quans, "\n")
-# cat("Overall sum", sum(gr_sums), "\n")
-# cat("Overall mean", mean(gr_sums / gr_quans), "\n")
-# cat("df_tr", df_tr, "\n")
-# cat("df_e", df_e, "\n")
-# cat("df_t", df_t, "\n")
-# cat("SSTr", SSTr, "\n")
-# cat("SSE", SSE, "\n")
-# cat("SST", SST, "\n")
-# cat("MSTr", MSTr, "\n")
-# cat("MSE", MSE, "\n")
-# cat("F", F, "\n")
+# Console output
+cat("Sums", gr_sums, "\n")
+cat("Averages", gr_sums / gr_quans, "\n")
+cat("Overall sum", sum(gr_sums), "\n")
+cat("Overall mean", mean(gr_sums / gr_quans), "\n")
+cat("df_tr", df_tr, "\n")
+cat("df_e", df_e, "\n")
+cat("df_t", df_t, "\n")
+cat("SSTr", SSTr, "\n")
+cat("SSE", SSE, "\n")
+cat("SST", SST, "\n")
+cat("MSTr", MSTr, "\n")
+cat("MSE", MSE, "\n")
+cat("F", F, "\n")
